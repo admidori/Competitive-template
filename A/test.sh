02599f77bee596c5815a6268b9c8a92a46efd80c
rp-agota <rootpass1824@gmail.com>
2021-03-20 18:54:49 +0900

    change test.sh file

--- A/test.sh
+++ A/test.sh
@@ -1,20 +1,65 @@
 #!/bin/sh
+CMDNAME=`basename $0`
+
+while getopts ab:c: OPT
+do
+  case $OPT in
+    "a" ) FLG_1="TRUE" ;;
+    "b" ) FLG_2="TRUE" ;;
+    "c" ) FLG_3="TRUE" ;;
+    "d" ) FLG_4="TRUE" ;;
+    "e" ) FLG_5="TRUE" ;;
+      * ) echo "Usage: $CMDNAME [-a] [-b] [-c] [-d] [-e]" 1>&2
+          exit 1 ;;
+  esac
+done
+
 echo "--------gcc error log----------"
 g++ -o solve sol.cpp
 printf "\n"
+
+if [ "$FLG_1" = "TRUE" ]; then
+  echo "----------case1.txt------------"
+  ./solve <case1.txt
+fi
+
+if [ "$FLG_2" = "TRUE" ]; then
+  echo "----------case2.txt------------"
+  ./solve <case2.txt
+fi
+
+if [ "$FLG_3" = "TRUE" ]; then
+  echo "----------case3.txt------------"
+  ./solve <case3.txt
+fi
+
+if [ "$FLG_4" = "TRUE" ]; then
+  echo "----------case4.txt------------"
+  ./solve <case4.txt
+fi
+
+if [ "$FLG_5" = "TRUE" ]; then
+  echo "----------case5.txt------------"
+  ./solve <case5.txt
+fi
+
+if [ "$1" = "" ]; then
 echo "----------case1.txt------------"
 ./solve <case1.txt
-printf "\n"
+
 echo "----------case2.txt------------"
 ./solve <case2.txt
-printf "\n"
+
 echo "----------case3.txt------------"
 ./solve <case3.txt
-printf "\n"
+
 echo "----------case4.txt------------"
 ./solve <case4.txt
-printf "\n"
+
 echo "----------case5.txt------------"
 ./solve <case5.txt
-printf "\n"
-echo "----------all case end!--------"
+fi
+
+echo "-------------END---------------"
+
+exit 0
