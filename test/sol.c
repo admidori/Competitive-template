593201b32d27fe572ea7ecfcd8c4a263d9dd8f50
rp-agota <53311906+rp-agota@users.noreply.github.com>
2021-08-09 21:30:49 +0900

    DEBUG function add.

--- test/sol.c
+++ test/sol.c
@@ -9,7 +9,8 @@
 
 #define DBG_INT(arg) fprintf(stderr,"*DEBUG* " #arg "...%d\n",arg)
 #define DBG_DOUBLE(arg) fprintf(stderr,"*DEBUG* " #arg "...%f\n",arg)
-#define DBG_CHAR(arg) fprintf(stderr,"*DEBUG* " #arg "...%s\n",arg)
+#define DBG_CHAR(arg) fprintf(stderr,"*DEBUG* " #arg "...%c\n",arg)
+#define DBG_STRING(arg) fprintf(stderr,"*DEBUG* " #arg "...%s\n",arg)
 
 int main(void){
 	
